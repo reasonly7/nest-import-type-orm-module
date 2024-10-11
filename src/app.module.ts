3b1a000a1c050827b23f2a10c2b1bc4fb5807c94
reasonly8 <reasonly8@sina.com>
2024-10-11 15:02:32 +0800

    feat: add importTypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { importConfigModule } from './config/importConfigModule';
+import { importTypeOrmModule } from './typeorm/importTypeOrmModule';
 
 @Module({
-  imports: [importConfigModule()],
+  imports: [importConfigModule(), importTypeOrmModule()],
   controllers: [AppController],
   providers: [AppService],
 })
